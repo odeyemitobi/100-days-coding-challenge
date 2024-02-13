59fd8f042710546cbec0e205c17a8d9b8fe720be
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2024-02-13 22:22:33 +0100

    Day 16 done

---
+++ Day 16/README.md
@@ -0,0 +1,2 @@
+Today i learnt about random module
+and used it to build a program that flips a coin and the random module picks either heads or tails
---
+++ Day 16/day16.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+flipped_coin = random.randint(0, 1)
+if flipped_coin == 1:
+    print("Heads")
+else:
+    print("Tails")
